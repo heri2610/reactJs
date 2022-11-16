9d4c05b84c2399f5cabb4cfc044af6c0da5756c4
heri2610 <suhaeriheri45@gmail.com>
2022-11-16 10:36:50 +0700

    menghilangkan rect-google-login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-import Navbar from './components/UI/NavigationBar';
-import Hero from './components/UI/Hero';
-import Footer from './components/UI/Footer';
-import LandingPage from './components/LandingPage/index';
-import SearchCar from './components/SearchCar/index';
+import Navbar from "./components/UI/NavigationBar";
+import Hero from "./components/UI/Hero";
+import Footer from "./components/UI/Footer";
+import LandingPage from "./components/LandingPage/index";
+import SearchCar from "./components/SearchCar/index";
 
 function App() {
-  const tokenz = window.localStorage.getItem("accessToken");
-
   return (
     <BrowserRouter>
       <Navbar />
       <Hero />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/cars" element={tokenz ? < SearchCar /> : <Navigate to="/" />} />
+        <Route path="/cars" element={<SearchCar />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/components/UI/Hero.js
+++ src/components/UI/Hero.js
@@ -1,21 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { Container, Row, Col } from 'react-bootstrap';
-import GoogleLogin from 'react-google-login';
+import { Container, Row, Col } from "react-bootstrap";
 import { gapi } from "gapi-script";
-import MyButton from './MyButton';
-import ErrorModal from './ErrorModal';
-import car from '../../assets/general/img_car.png';
+import MyButton from "./MyButton";
+import ErrorModal from "./ErrorModal";
+import car from "../../assets/general/img_car.png";
 
 const Hero = () => {
   const location = useLocation();
-  const tokenz = window.localStorage.getItem("accessToken");
   const [errorModal, setErrorModal] = useState(false);
-
-  const responseGoogle = response => {
-    window.localStorage.setItem("accessToken", response.accessToken);
-    window.location.reload();
-  };
 
   useEffect(() => {
     function start() {
@@ -37,22 +30,12 @@ const Hero = () => {
               <Col lg={6} className="welcome d-flex align-items-center">
                 <div className="welcome-text">
                   <p className="slogan fw-bold">Sewa & Rental Mobil Terbaik di kawasan Surabaya</p>
-                  <p className="fw-light">Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas dengan harga
-                    terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.</p>
+                  <p className="fw-light">Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas dengan harga terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.</p>
                   {location.pathname === "/" && (
                     <div>
-                      {tokenz ?
-                        <Link to={"/cars"}><MyButton>Mulai Sewa Mobil</MyButton></Link>
-                        : <MyButton onClick={() => setErrorModal(true)}>Mulai Sewa Mobil</MyButton>
-                      }
-                      <GoogleLogin
-                        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                        buttonText="Login with Google"
-                        onSuccess={responseGoogle}
-                        onFailure={responseGoogle}
-                        cookiePolicy={"single_host_origin"}
-                        className="ms-4"
-                      />
+                      <Link to={"/cars"}>
+                        <MyButton>Mulai Sewa Mobil</MyButton>
+                      </Link>
                     </div>
                   )}
                 </div>
@@ -65,13 +48,8 @@ const Hero = () => {
             </Row>
           </Container>
         </div>
-      </div >
-      <ErrorModal
-        show={errorModal}
-        title='Unauthorized'
-        message='Please login with your Google account.'
-        onHide={() => setErrorModal(false)}
-      />
+      </div>
+      <ErrorModal show={errorModal} title="Unauthorized" message="Please login with your Google account." onHide={() => setErrorModal(false)} />
     </>
   );
 };
